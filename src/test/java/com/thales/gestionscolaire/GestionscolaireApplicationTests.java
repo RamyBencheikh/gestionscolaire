6f18f5c1e43b2c87bdb6766eb4fab221dbc28dcb
BEN CHEIKH Ramy <ramy.ben-cheikh@thalesgroup.com>
2023-02-02 13:58:55 +0100

    First commit

---
+++ src/test/java/com/thales/gestionscolaire/GestionscolaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thales.gestionscolaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionscolaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
